0534ce23c918f1490a7293c632f898238453c6e5
sacha11 <s.j.g_97@hotmail.com>
2024-10-30 18:01:18 -0300

    navigation component built

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import  TasksPage  from "./pages/TasksPage.jsx";
 import TaskFormPage from "./pages/TaskFormPage.jsx";
+import Navigation from './components/Navigation.jsx';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Navigation/>
       <Routes>
         <Route path='/' element={<Navigate to="/tasks" />} />
         <Route path='/tasks' element={<TasksPage />} />
