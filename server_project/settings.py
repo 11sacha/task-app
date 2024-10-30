646fc720d7f2b3d76ea90e8924bba1fe2c1120a2
sacha11 <s.j.g_97@hotmail.com>
2024-10-30 16:18:18 -0300

    added back-end documentation

--- server_project/settings.py
+++ server_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'corsheaders',
+    'coreapi',
     'rest_framework',
     'tasks_app'
 ]
@@ -130,3 +131,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
--- tasks_app/urls.py
+++ tasks_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
-from tasks import views
+from tasks_app import views
+from rest_framework.documentation import include_docs_urls
 
 #Api versioning
 router = routers.DefaultRouter()
 router.register(r'tasks', views.TaskView, 'tasks')
 
 urlpatterns = [
-    path("api/v1", include(router.urls))
+    path("api/v1/", include(router.urls)),
+    path('docs/', include_docs_urls(title='Task_Api')),
 ]
