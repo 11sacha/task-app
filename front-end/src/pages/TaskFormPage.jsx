6716556deb711143b749343e1a14214e240bfedb
sacha11 <s.j.g_97@hotmail.com>
2024-11-04 18:49:56 -0300

    taskformpage updated.. in dev

--- front-end/src/pages/TaskFormPage.jsx
+++ front-end/src/pages/TaskFormPage.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import useForm from "react-hook-form";
 
 const TaskFormPage = () => {
+  const { register } = useForm();
+
   return (
     <div>
       <form action="">
-        <input type="text" placeholder='Title' />
-        <textarea rows="3" placeholder="Description"></textarea>
+        <input 
+          type="text" 
+          placeholder='Title'
+          {...register("title", { required: true })} 
+        />
+        <textarea 
+          rows="3" 
+          placeholder="Description"
+          {...register("description", { required: true })} 
+        ></textarea>
         <button>Save</button>
       </form>
     </div>
